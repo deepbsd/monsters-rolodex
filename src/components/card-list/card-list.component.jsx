add76505a5f243cfb63957b202a45986ab9693ae
David S. Jackson <deepbsd@yahoo.com>
2019-09-11 17:11:07 -0400

    filtering is working now... added a search-box component for reusability

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,13 +5,15 @@ import { Card } from '../card/card.component';
 
 
 
-
 export const CardList = props => (
 
     <div className="card-list">
 
         { props.monsters.map( monster => 
-            ( <Card key={monster.id} monster={monster} /> )) }
+
+            ( <Card key={monster.id} monster={monster} /> )) 
+
+        }
 
     </div>
 
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -8,6 +8,8 @@ export const Card = props => (
         <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
 
         <h2>{props.monster.name}</h2>
+
         <p>{props.monster.email}</p>
+
     </div>
 )
