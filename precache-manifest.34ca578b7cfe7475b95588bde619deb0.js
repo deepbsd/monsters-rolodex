46f85dc7a1758b74f3316131f1cab3444071010c
David S. Jackson <deepbsd@yahoo.com>
2019-09-12 14:05:56 -0400

    Updates

---
+++ precache-manifest.34ca578b7cfe7475b95588bde619deb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eea89633702f529ff4acb0488af237",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0554e8f383cd8d2af903",
+    "url": "/monsters-rolodex/static/css/main.15699484.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "0554e8f383cd8d2af903",
+    "url": "/monsters-rolodex/static/js/main.7629e841.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex.git/precache-manifest.33a180b445617186b5af732362f79977.js"
+  "/monsters-rolodex/precache-manifest.34ca578b7cfe7475b95588bde619deb0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
