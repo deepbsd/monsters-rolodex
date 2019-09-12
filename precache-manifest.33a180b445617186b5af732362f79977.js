48ab5dfe90a192ffd8ecbd3053e2cc58ec61342a
David S. Jackson <deepbsd@yahoo.com>
2019-09-12 12:34:49 -0400

    Updates

---
+++ precache-manifest.33a180b445617186b5af732362f79977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d0bd2306c74670daf3be4daef56f01",
+    "url": "/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "513247883e68ff8be9bc",
+    "url": "/monsters-rolodex.git/static/css/main.15699484.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex.git/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "513247883e68ff8be9bc",
+    "url": "/monsters-rolodex.git/static/js/main.d913ae34.chunk.js"
+  },
+  {
+    "revision": "6d9d714627a100d3f6d6",
+    "url": "/monsters-rolodex.git/static/js/runtime~main.c475ca6c.js"
+  }
+]);
